9485e721dbbf40d51018dc6ff4ce9c3d666399d1
Vazgen Gasparyan <vlashebnik777@gmail.com>
2022-01-03 19:47:10 +0400

    new additions to myfile.py file

--- myfile.py
+++ myfile.py
@@ -5,3 +5,14 @@ st.title("Merry Christmas and Happy New Year!")
 
 image = Image.open('img/santa.jpg')
 st.image(image, width=700)
+
+st.write('“Gifts come and go, what really matters are the people who light up our lives all year long. Thank you!”')
+
+def factorial(n):
+    if n in (0, 1): return 1
+    else: return n * factorial(n-1)
+
+n = st.slider('Select the number.')
+st.write('The factorial of {0} is {1}.'.format(n, factorial(n)))
+
+st.text_input("Enter your name")
